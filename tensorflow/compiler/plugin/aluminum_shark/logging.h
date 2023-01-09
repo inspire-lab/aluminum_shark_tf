6536bd908ad0d8d3a35d6414c1d2c08cf0d539c4
Robert Podschwadt <robertpodschwadt@gmail.com>
2023-01-09 13:17:00 -0700

    added logging prefix

--- tensorflow/compiler/plugin/aluminum_shark/logging.h
+++ tensorflow/compiler/plugin/aluminum_shark/logging.h
@@ -32,6 +32,9 @@ bool log(int level);
 
 void set_log_level(int level);
 
+void set_log_prefix(const std::string& prefix);
+const std::string& get_log_prefix();
+
 NullStream& nullstream();
 
 constexpr int AS_CRITICAL = 50;
@@ -47,36 +50,42 @@ constexpr int AS_DEBUG = 10;
 // streaming interface
 #define AS_LOG_S                                                         \
   (::aluminum_shark::log() ? std::cout : ::aluminum_shark::nullstream()) \
-      << "Aluminum Shark: " << __FILE__ << ":" << __LINE__ << "] "
+      << ::aluminum_shark::get_log_prefix() << ": " << __FILE__ << ":"   \
+      << __LINE__ << "] "
 // append to stream
 #define AS_LOG_SA \
   (::aluminum_shark::log() ? std::cout : ::aluminum_shark::nullstream())
 
-#define AS_LOG_CRITICAL                                 \
-  (::aluminum_shark::log(::aluminum_shark::AS_CRITICAL) \
-       ? std::cout                                      \
-       : ::aluminum_shark::nullstream())                \
-      << "Aluminum Shark CRITICAL: " << __FILE__ << ":" << __LINE__ << "] "
-#define AS_LOG_ERROR                                 \
-  (::aluminum_shark::log(::aluminum_shark::AS_ERROR) \
-       ? std::cout                                   \
-       : ::aluminum_shark::nullstream())             \
-      << "Aluminum Shark ERROR: " << __FILE__ << ":" << __LINE__ << "] "
-#define AS_LOG_WARNING                                 \
-  (::aluminum_shark::log(::aluminum_shark::AS_WARNING) \
-       ? std::cout                                     \
-       : ::aluminum_shark::nullstream())               \
-      << "Aluminum Shark WARNING: " << __FILE__ << ":" << __LINE__ << "] "
-#define AS_LOG_INFO                                 \
-  (::aluminum_shark::log(::aluminum_shark::AS_INFO) \
-       ? std::cout                                  \
-       : ::aluminum_shark::nullstream())            \
-      << "Aluminum Shark INFO: " << __FILE__ << ":" << __LINE__ << "] "
-#define AS_LOG_DEBUG                                 \
-  (::aluminum_shark::log(::aluminum_shark::AS_DEBUG) \
-       ? std::cout                                   \
-       : ::aluminum_shark::nullstream())             \
-      << "Aluminum Shark DEBUG: " << __FILE__ << ":" << __LINE__ << "] "
+#define AS_LOG_CRITICAL                                                  \
+  (::aluminum_shark::log(::aluminum_shark::AS_CRITICAL)                  \
+       ? std::cout                                                       \
+       : ::aluminum_shark::nullstream())                                 \
+      << ::aluminum_shark::get_log_prefix() << " CRITICAL: " << __FILE__ \
+      << ":" << __LINE__ << "] "
+#define AS_LOG_ERROR                                                         \
+  (::aluminum_shark::log(::aluminum_shark::AS_ERROR)                         \
+       ? std::cout                                                           \
+       : ::aluminum_shark::nullstream())                                     \
+      << ::aluminum_shark::get_log_prefix() << " ERROR: " << __FILE__ << ":" \
+      << __LINE__ << "] "
+#define AS_LOG_WARNING                                                         \
+  (::aluminum_shark::log(::aluminum_shark::AS_WARNING)                         \
+       ? std::cout                                                             \
+       : ::aluminum_shark::nullstream())                                       \
+      << ::aluminum_shark::get_log_prefix() << " WARNING: " << __FILE__ << ":" \
+      << __LINE__ << "] "
+#define AS_LOG_INFO                                                         \
+  (::aluminum_shark::log(::aluminum_shark::AS_INFO)                         \
+       ? std::cout                                                          \
+       : ::aluminum_shark::nullstream())                                    \
+      << ::aluminum_shark::get_log_prefix() << " INFO: " << __FILE__ << ":" \
+      << __LINE__ << "] "
+#define AS_LOG_DEBUG                                                         \
+  (::aluminum_shark::log(::aluminum_shark::AS_DEBUG)                         \
+       ? std::cout                                                           \
+       : ::aluminum_shark::nullstream())                                     \
+      << ::aluminum_shark::get_log_prefix() << " DEBUG: " << __FILE__ << ":" \
+      << __LINE__ << "] "
 
 #endif /* ALUMINUM_SHARK_DEPENDENCIES_TENSORFLOW_TENSORFLOW_COMPILER_PLUGIN_ALUMINUM_SHARK_LOGGING_H \
         */
