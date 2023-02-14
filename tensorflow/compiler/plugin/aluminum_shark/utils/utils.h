ae6e09d11ed9843ea233278e26e793231b933db7
Robert Podschwadt <robertpodschwadt@gmail.com>
2023-02-13 17:38:03 -0700

    more generic logging for iterables

--- tensorflow/compiler/plugin/aluminum_shark/utils/utils.h
+++ tensorflow/compiler/plugin/aluminum_shark/utils/utils.h
@@ -4,14 +4,41 @@
 #include <iomanip>
 #include <ios>
 #include <iostream>
+#include <type_traits>
 #include <vector>
 
 #include "tensorflow/compiler/plugin/aluminum_shark/logging.h"
 
+namespace impl {
+// see:
+// https://stackoverflow.com/questions/13830158/check-if-a-variable-type-is-iterable
+// To allow ADL with custom begin/end
+using std::begin;
+using std::end;
+
+template <typename T>
+auto is_iterable_impl(int)
+    -> decltype(begin(std::declval<T&>()) !=
+                    end(std::declval<T&>()),  // begin/end and operator !=
+                void(),                       // Handle evil operator ,
+                ++std::declval<
+                    decltype(begin(std::declval<T&>()))&>(),  // operator
+                                                              // ++
+                void(*begin(std::declval<T&>())),             // operator*
+                std::true_type{});
+
+template <typename T>
+std::false_type is_iterable_impl(...);
+
+}  // namespace impl
+
 namespace aluminum_shark {
 
+template <typename T>
+using is_iterable = decltype(impl::is_iterable_impl<T>(0));
+
 template <class T>
-std::ostream& stream_vector(const std::vector<T>& vec, size_t n = 0,
+std::ostream& stream_vector(const T& vec, size_t n = 0,
                             std::ostream& os = std::cout) {
   n = n == 0 || n > vec.size() ? vec.size() : n;
   os << "[";
@@ -46,11 +73,6 @@ void print_vector(const std::vector<T>& vec, size_t n = 0,
 
 using Shape = std::vector<size_t>;
 
-// converts a python sytle index [x,y,z] into single index
-// into the flat sotrage vector
-size_t multi_index_to_flat(const std::vector<size_t>& index,
-                           const Shape& shape);
-
 // wrapper that allows us to overload << for shape only
 class ShapePrint {
  public:
@@ -58,7 +80,53 @@ class ShapePrint {
   const Shape& shape;
 };
 
+// wrapper so we can print unkonws iterable type without introdcuing
+// dependencies liske absl::Span
+template <class T>
+class IterablePrintWrapper {
+ public:
+  IterablePrintWrapper(const T& t) : value(t){};
+  const T& value;
+};
+
+// converts a python sytle index [x,y,z] into single index
+// into the flat sotrage vector
+template <class T, class S>
+size_t multi_index_to_flat(const T& index, const S& shape) {
+  if (shape.size() != index.size()) {
+    AS_LOG_CRITICAL << "missmatching index: " << IterablePrintWrapper<T>(index)
+                    << " and shape: " << IterablePrintWrapper<S>(shape)
+                    << std::endl;
+    throw std::invalid_argument("index and shape missmatch");
+  }
+  // const size_t = index.size();
+  size_t ret = 0;
+  size_t scale = 1;
+  for (size_t dim = index.size(); dim-- != 0;) {
+    if (index[dim] >= shape[dim]) {
+      AS_LOG_CRITICAL << "index: " << IterablePrintWrapper<T>(index)
+                      << " and shape " << IterablePrintWrapper<S>(shape)
+                      << " missmatch " << std::endl;
+      throw std::invalid_argument("index and shape missmatch");
+    }
+    ret += scale * index[dim];
+    scale *= shape[dim];
+  }
+  return ret;
+};
+
 std::ostream& operator<<(std::ostream& os, const ShapePrint& shape);
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const IterablePrintWrapper<T>& ipw) {
+  const T& t = ipw.value;
+  os << "[";
+  for (size_t i = 0; i < t.size(); ++i) {
+    os << t[i] << (i != t.size() - 1 ? ", " : "");
+  }
+  os << "]";
+  return os;
+};
 
 template <class T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
@@ -103,8 +171,8 @@ std::ostream& operator<<(std::ostream& os, const PrintWithShape<T>& printer) {
     size_t closing_brackets = 0;
     int div = 1;
     // check if we need to make closing brackets, calculating the product over
-    // the dimensions. if it any point it divides the index without remainder we
-    // need a closing ]. if we have any ] we also need a \n
+    // the dimensions. if it any point it divides the index without remainder
+    // we need a closing ]. if we have any ] we also need a \n
     for (size_t j = s.size(); j-- != 0;) {
       div *= s[j];
       closing_brackets += (i % div == 0);  // true converts to 1, false to zero
