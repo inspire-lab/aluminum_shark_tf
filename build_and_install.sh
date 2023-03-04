e8ba0935ca7c0d889b82f4259b71fc1cf0bf5e75
Robert Podschwadt <robertpodschwadt@gmail.com>
2023-03-04 00:27:30 -0700

    made number of jobs configurable

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+N_JOBS=$1
+if [ -z $N_JOBS ]; then
+    N_JOBS=6
+fi
+
 #bazel build --local_ram --config=dbg resources=4096 --subcommands //tensorflow/tools/pip_package:build_pip_package --verbose_failures
 # bazel build -j 6 --config=dbg //tensorflow/tools/pip_package:build_pip_package #--verbose_failures
-bazel build -j 6  --local_ram_resources=4096  //tensorflow/tools/pip_package:build_pip_package --verbose_failures
+bazel build -j $N_JOBS  --local_ram_resources=4096  //tensorflow/tools/pip_package:build_pip_package --verbose_failures
 if [ $? -ne 0 ]; then
     exit 1
 fi
