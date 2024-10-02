12e70b7ba1a27af63ba26f4f42ba2f81ae44da16
Robert Podschwadt <32943591+podschwadt@users.noreply.github.com>
2024-10-02 12:41:14 -0600

    Update build_and_install.sh

--- build_and_install.sh
+++ build_and_install.sh
@@ -7,7 +7,7 @@ fi
 
 #bazel build --local_ram --config=dbg resources=4096 --subcommands //tensorflow/tools/pip_package:build_pip_package --verbose_failures
 # bazel build -j 6 --config=dbg //tensorflow/tools/pip_package:build_pip_package #--verbose_failures
-bazel build -j $N_JOBS  --local_ram_resources=4096  //tensorflow/tools/pip_package:build_pip_package --verbose_failures
+bazel build -j $N_JOBS   //tensorflow/tools/pip_package:build_pip_package --verbose_failures
 if [ $? -ne 0 ]; then
     exit 1
 fi
@@ -17,5 +17,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-pip install --no-deps --force-reinstall  /tmp/tensorflow_pkg/tensorflow-2.7.0-cp38-cp38-linux_x86_64.whl 
-# && pip install numpy==1.21.2
+# determine the numpy version that used to build
+np_version=`python -c "import numpy as np; print(np.__version__)"`
+
+pip install --force-reinstall  /tmp/tensorflow_pkg/tensorflow-2.7.0-cp38-cp38-linux_x86_64.whl && pip install numpy==$np_version
